f16fac69afa76eb2954b53fd8dbef08fd36f2d85
Federico Giuggioloni <federico.giuggioloni@gmail.com>
2016-04-06 17:56:23 +0100

    Reorganized project, created some of the server-side View classes

--- pynative/pynative.py
+++ pynative/pynative.py
@@ -16,78 +16,73 @@ class PyNative():
     def getHeader(self, value):
         return request.headers.get(value)
 
-class ElementBuilder():
-
-    json = {}
-
-    def __init__(self, jsoninit):
-        self.json = jsoninit
-
-    @classmethod
-    def createTextView(cls, el_id, text):
-        json = {
-                "type": "textview",
-                "id": el_id,
-                "text": text
-            }
-
-        return cls(json)
-
-    @classmethod
-    def createGotoButton(cls, el_id, text, toScreen):
-        json = {
-                "type": "button",
-                "id": el_id,
-                "text": text,
-                "action": {
-                    "type": "goto",
-                    "screen": toScreen
-                    }
-                }
-
-        return cls(json)
-
-    def getAsDict(self):
-        return self.json
-
-
-class ViewBuilder():
-
-    element_list = []
-
-    def __init__(self, context, view_screenname = "MainScreen", view_title = "PyNative!"):
-        self.context = context
-        self.view_screenname = view_screenname
-        self.view_title = view_title
-        self.element_list = []
-
-    def build(self):
-        mimetype = self.context.getHeader('Content-Type')
+    def render(self, screen):
+        mimetype = self.getHeader('Content-Type')
 
         result = "unknown"
         if mimetype == "application/json":
-            result = self.toJson()
+            result = screen.get_json()
         else:
-            result = self.toHtml()
+            json = screen.get_json()
+            result = render_template("index.html", data = json, pagetitle = screen.data['title'])
         return result
 
+#class ElementBuilder():
+#
+#    json = {}
+#
+#    def __init__(self, jsoninit):
+#        self.json = jsoninit
+#
+#    @classmethod
+#    def createTextView(cls, el_id, text):
+#        json = {
+#                "type": "textview",
+#                "id": el_id,
+#                "text": text
+#            }
+#
+#        return cls(json)
+#
+#    @classmethod
+#    def createGotoButton(cls, el_id, text, toScreen):
+#        json = {
+#                "type": "button",
+#                "id": el_id,
+#                "text": text,
+#                "action": {
+#                    "type": "goto",
+#                    "screen": toScreen
+#                    }
+#                }
+#
+#        return cls(json)
+#
+#    def getAsDict(self):
+#        return self.json
 
-    def getAsDict(self):
-        return {
-                "screenname": self.view_screenname,
-                "title": self.view_title,
-                "layout":{
-                    "elements":self.element_list
-                    }
-                }
-
-    def addElement(self, element):
-        self.element_list.append(element.getAsDict())
 
 
-    def toJson(self):
-        return json.dumps(self.getAsDict())
-
-    def toHtml(self):
-        data = self.toJson()
-        return render_template("index.html", data = data, screentitle = self.view_title)
+#class ViewBuilder():
+#
+#    def __init__(self, context, screen):
+#        self.context = context
+#        self.screen = screen
+#
+#    def build(self):
+#        mimetype = self.context.getHeader('Content-Type')
+#
+#        result = "unknown"
+#        if mimetype == "application/json":
+#            result = self.toJson()
+#        else:
+#            result = self.toHtml()
+#        return result
+#
+#
+#    def getAsDict(self):
+#        return self.screen.data
+#
+#    def toHtml(self):
+#        data = self.toJson()
+#        return render_template("index.html", data = data, screentitle = self.view_title)
---
+++ pynative/views.py
@@ -0,0 +1,111 @@
+import json
+
+#   Target JSON:
+#   {
+#       "screenname": name,
+#       "title": title,
+#       "layout": {
+#           "views": [
+#               <element1>,
+#               <element2>,
+#               . . .,
+#               <elementN>
+#           ]
+#       }
+#   }
+#
+#   <element> :
+#
+#   {
+#       "type": type, //one of the classnames (ex.: view, textview...)
+#       "id": id, //If not specified, __
+#       "parameters": {
+#           <type-specific parameters>
+#       }
+#   }
+#
+#
+
+class Screen(object):
+    def __init__(self, name = "MainScreen", title = "PyNative!"):
+        self.data = {
+                "screenname": name,
+                "title": title,
+                "layout": {
+                    "views": []
+                    }
+                }
+
+        self.views = []
+
+    def add_view(self, view):
+        self.views.append(view)
+        self.data['layout']['views'].append(view.data)
+        print "Data is %s" % json.dumps(self.data)
+
+    def get_type(self):
+        return self.__name__.lower()
+
+    def get_data(self):
+        return self.data
+
+    def get_json(self):
+        return json.dumps(self.data)
+
+class View(object):
+
+    def __init__(self, vid = "__", start_data = {}):
+        start_data['id'] = vid
+        start_data['type'] = self.get_type()
+        start_data['parameters'] = {}
+        start_data['parameters']['width'] = 'wrap_content'  #TODO implement
+        start_data['parameters']['height'] = 'wrap_content' #TODO implement
+        start_data['parameters']['events'] = {}
+
+        self.data = start_data
+
+    def get_type(self):
+        return type(self).__name__.lower()
+
+    def get_data(self):
+        return self.data
+
+    def get_json(self):
+        return json.dumps(self.data)
+
+class TextView(View):
+
+    def __init__(self, text = "TextView", vid = "__", start_data = {}):
+        super(TextView, self).__init__(vid = vid, start_data = start_data)
+
+        self.data['parameters']['text'] = text
+
+    def set_text(self, text):
+        self.data['parameters']['text'] = text
+
+class Button(TextView):
+
+    def __init__(self, text = "Button", vid = "__", start_data = {}):
+        super(Button, self).__init__(text = text, vid = vid, start_data = start_data)
+
+    def add_event(self, event_type, event_action):   #TODO create a class Action for event_action?
+        """ Adds an event to specified Button
+        event_type is the type of the event (onclick, onlongclick...)
+        event_action is what to do when pressed (goto, do...)
+        """
+
+        self.data['parameters']['events'].update({ event_type: event_action })
+
+class ViewGroup(View):
+
+    def __init__(self, vid = "__", start_data = {}):
+        super(ViewGroup, self).__init__(vid = vid, start_data = start_data)
+
+        self.data['parameters']['layout'] = {}
+        self.data['parameters']['layout']['views'] = []
+        self.views = []
+
+    def add_element(view):
+        self.views.append(view)
+        self.data['parameters']['layout']['views'].append(view.data)
+
