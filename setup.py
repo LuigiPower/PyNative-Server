2fd81d079b5165ed6ead43bc8c7c03b33816db94
Federico Giuggioloni <federico.giuggioloni@gmail.com>
2016-04-20 17:06:39 +0100

    setup

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     version='0.0.3',
     description='PyNative library',
     long_description=readme,
+    license=license,
     author='Federico Giuggioloni',
     author_email='federico.giuggioloni@gmail.com',
     url='https://github.com/LuigiPower/PyNative-Server',
@@ -22,7 +23,6 @@ setup(
     packages=find_packages(exclude=('tests', 'docs')),
     include_package_data=True,
     zip_safe=False,
-    install_requires=['Flask', 'Flask-Triangle'],
-    license=license
+    install_requires=['Flask', 'Flask-Triangle']
 )
 
